a4ac40ecb353d861bba3db5ac55fd1f11a885197
Wayosi <emmanuelquao@gmail.com>
2017-02-24 19:54:42 +0000

    Assignment 2

--- question1.py
+++ question1.py
@@ -3,9 +3,15 @@
 #Name: Quao Nartey-wayo Emmanuel
 #Index Number: PS/ITC/14/0020
 
-
+#create array to store the numbers temporarily
 num_list=[]
+
+#check for numbers within the range that satisfy the conditions given
 for number in range (1000, 7001):
      if (number%7==0) and (number%5!=0):
+
+#add the required numbers to the 'num_list' array
           num_list.append(str(number))
+
+#print out the numbers stored temporarily in the array
 print (','.join(num_list))
--- question3.py
+++ question3.py
@@ -3,12 +3,12 @@
 #Name: Quao Nartey-Wayo Emmanuel
 #Index Number:PS/ITC/14/0020
 
-import sys
 
-def convertToBinary (number):
+
+def convertToBinary ():
+     number = input("Please enter a decimal number: ")
      decimal = int(number)
      print (decimal, "in binary = ",bin(decimal) )
 
 if __name__ =='__main__':
-     for arg in sys.argv[1:]:
-         convertToBinary(arg)
+      convertToBinary()
